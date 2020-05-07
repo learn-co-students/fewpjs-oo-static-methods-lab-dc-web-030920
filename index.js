adb301327cf1b2bfadcd94281b6341f37af45b15
lmonty22 <lindsay.montgomery@olympian.org>
2020-05-07 14:02:50 -0400

    halfway through lab

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    let array = string.split("")
+    array[0] = string[0].toUpperCase()
+    return array.join("")
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  static titleize(sent){
+    // return sent.split(' ').forEach(word => this.capitalize(word)).join('')
+  }
 }
