3e9dc416e58f64e7918a78cea590682b4b8b763f
lmonty22 <lindsay.montgomery@olympian.org>
2020-05-07 15:06:53 -0400

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ class Formatter {
     return string.replace(/[^A-Za-z0-9-' ]+/g, '');
   }
   static titleize(sent){
-    // return sent.split(' ').forEach(word => this.capitalize(word)).join('')
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const array = sent.split(' ')
+    const new_array = []
+    array.forEach((word) => {
+      if (exceptions.includes(word)){
+        new_array.push(word)
+      }else{new_array.push(this.capitalize(word))}
+  })
+  new_array[0] = this.capitalize(new_array[0])
+  return new_array.join(' ')
   }
 }
